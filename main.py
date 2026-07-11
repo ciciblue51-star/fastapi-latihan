4b62e65ff2e7b359ba13cc60f18619bc422a6c9d
ciciblue51-star <ciciblue51@gmail.com>
2026-07-11 18:49:27 +0800

    Update main.py Tambah path parameter (Soal 2)

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello FastAPI"}
+    
+@app.get("/students/{student_id}")
+def get_student(student_id: int):
+    return {"student_id": student_id}
