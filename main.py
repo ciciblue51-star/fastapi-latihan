de517777b11c2d8a7f477aab2ee438f7c6ffdb21
ciciblue51-star <ciciblue51@gmail.com>
2026-07-11 18:50:56 +0800

    Update main.py Tambah query parameter (Soal 3)

--- main.py
+++ main.py
@@ -9,3 +9,7 @@ def read_root():
 @app.get("/students/{student_id}")
 def get_student(student_id: int):
     return {"student_id": student_id}
+
+@app.get("/search")
+def search(keyword: str, limit: int = 10):
+    return {"keyword": keyword, "limit": limit}
